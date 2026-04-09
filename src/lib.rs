60742d0c82952c74e30227de0bb9f7ed72435298
invuzt <invuzt@gmail.com>
2026-04-09 11:04:47 +0700

    Fix: Force android feature in GitHub Actions

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,8 @@ use android_activity::AndroidApp;
 fn android_main(app: AndroidApp) {
     let mut options = eframe::NativeOptions::default();
     
-    // Di eframe 0.27, app dimasukkan ke renderer_init_closure atau lewat cara ini:
+    // Trik: Memasukkan AndroidApp ke dalam closure renderer
+    // Ini menghindari masalah jumlah argumen pada run_native
     eframe::run_native(
         "Hello Egui",
         options,
@@ -20,22 +21,18 @@ struct MyApp {
 
 impl Default for MyApp {
     fn default() -> Self {
-        Self { name: "Odfiz User".to_owned() }
+        Self { name: "Odfiz".to_owned() }
     }
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.vertical_centered(|ui| {
-                ui.heading("Odfiz Hello World");
-                ui.add_space(10.0);
-                ui.label(format!("Halo, {}!", self.name));
-                ui.text_edit_singleline(&mut self.name);
-                if ui.button("Klik").clicked() {
-                    self.name = "Build Berhasil!".to_owned();
-                }
-            });
+            ui.heading("Odfiz Egui Android");
+            ui.text_edit_singleline(&mut self.name);
+            if ui.button("Klik").clicked() {
+                self.name = "BERHASIL!".to_owned();
+            }
         });
     }
 }
