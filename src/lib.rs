98b13508743037ad190f2cf5e6839aa7f77b0746
invuzt <invuzt@gmail.com>
2026-04-09 11:22:03 +0700

    Final move: switch to run_android to end argument war

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
 #![cfg(target_os = "android")]
 use eframe::egui;
 
-// Ambil AndroidApp langsung dari crate-nya, bukan lewat eframe
 #[no_mangle]
 fn android_main(app: android_activity::AndroidApp) {
     let options = eframe::NativeOptions::default();
 
-    eframe::run_native(
-        "Odfiz App",
+    // Fungsi ini khusus untuk Android, argumennya pas (3 buah)
+    // app, options, closure
+    eframe::run_android(
+        app,
         options,
         Box::new(|_cc| Box::new(MyApp::default())),
-        app,
-    );
+    ).unwrap();
 }
 
 struct MyApp {
@@ -27,7 +27,7 @@ impl Default for MyApp {
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Akhirnya Berhasil!");
+            ui.heading("Odfiz Android Berhasil!");
             ui.label(format!("Halo, {}!", self.name));
             ui.text_edit_singleline(&mut self.name);
         });
