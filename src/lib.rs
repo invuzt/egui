129154ba0cfe2c37a2cb2045a05aacaad224cb67
invuzt <invuzt@gmail.com>
2026-04-09 11:25:58 +0700

    The actual final fix: EventLoopBuilder manual injection

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,22 @@
 use eframe::egui;
 
 #[no_mangle]
-fn android_main(app: android_activity::AndroidApp) {
-    let options = eframe::NativeOptions::default();
+fn android_main(app: winit::platform::android::activity::AndroidApp) {
+    use winit::platform::android::EventLoopBuilderExtAndroid;
 
-    // Fungsi ini khusus untuk Android, argumennya pas (3 buah)
-    // app, options, closure
-    eframe::run_android(
-        app,
+    let mut options = eframe::NativeOptions::default();
+    
+    // Kita buat event loop builder secara manual agar eframe "terpaksa" mengenalinya sebagai Android
+    options.event_loop_builder = Some(Box::new(move |builder| {
+        builder.with_android_app(app);
+    }));
+
+    // Di sini kita gunakan 3 argumen sesuai keinginan compiler Anda
+    eframe::run_native(
+        "Odfiz App",
         options,
         Box::new(|_cc| Box::new(MyApp::default())),
-    ).unwrap();
+    );
 }
 
 struct MyApp {
@@ -27,7 +33,7 @@ impl Default for MyApp {
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Odfiz Android Berhasil!");
+            ui.heading("Odfiz Build Success!");
             ui.label(format!("Halo, {}!", self.name));
             ui.text_edit_singleline(&mut self.name);
         });
