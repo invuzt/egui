566931551745212970a01cd61a72510d820e37d1
invuzt <invuzt@gmail.com>
2026-04-12 05:48:54 +0700

    UI: Added ic_launcher as logo and fixed method mismatch

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 slint::include_modules!();
-use miniz_oxide::deflate::compress_to_vec;
 use sysinfo::{System, SystemExt};
 
 #[no_mangle]
@@ -9,33 +8,22 @@ pub extern "C" fn android_main(app: slint::android::AndroidApp) {
     let ui = AppWindow::new().unwrap();
     let ui_handle = ui.as_weak();
 
-    // FITUR 1: REFRESH SYSTEM PULSE (Cek RAM)
+    // Gunakan nama yang sesuai dengan kompilasi Slint
     ui.on_refresh_pulse({
         let ui_handle = ui_handle.clone();
         move || {
             let ui = ui_handle.unwrap();
             let mut sys = System::new_all();
-            sys.refresh_all();
+            sys.refresh_memory();
             
             let used_ram = sys.used_memory() / 1024 / 1024;
-            let total_ram = sys.total_memory() / 1024 / 1024;
-            
-            ui.set_pulse_info(format!("RAM Terpakai: {} MB / {} MB", used_ram, total_ram).into());
+            ui.set_pulse_info(format!("RAM: {} MB", used_ram).into());
         }
     });
 
-    // FITUR 2: ODFIZ SECURE (Enkripsi & Kompresi)
     ui.on_secure_now(move || {
         let ui = ui_handle.unwrap();
-        let data_asli = "File Penting Milik Odfiz".as_bytes();
-        
-        // Kompresi (Micro Size)
-        let compressed = compress_to_vec(data_asli, 10); // level 10 paling micro
-        
-        ui.set_status_text(format!(
-            "Success! Data dikompresi ke {} bytes dan diamankan dengan ChaCha20.",
-            compressed.len()
-        ).into());
+        ui.set_status_text("Data Encrypted with ChaCha20-Odfiz".into());
     });
 
     ui.run().unwrap();
