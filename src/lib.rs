faf8f3db9d094c6b1c7a0017d733065ca71b1189
invuzt <invuzt@gmail.com>
2026-04-12 01:41:27 +0700

    Fix: Enable backend-android feature in Slint

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ slint::include_modules!();
 
 #[no_mangle]
 pub extern "C" fn android_main(app: slint::android::AndroidApp) {
-    // Inisialisasi wajib untuk Slint Android
+    // Sekarang slint::android harusnya sudah 'kenal' karena fitur sudah aktif
     slint::android::init(app).unwrap();
     
     let ui = AppWindow::new().unwrap();
