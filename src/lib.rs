798132e6e45b62cf190599a3d73174073c36460c
invuzt <invuzt@gmail.com>
2026-04-11 20:57:56 +0700

    Fix: Added explicit android-activity dependency and slint::android::init

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,16 @@
 slint::include_modules!();
 
-// Memastikan backend android ikut ter-link
+// Link backend android agar tidak hilang saat kompilasi
 #[cfg(target_os = "android")]
 use i_slint_backend_android_activity as _;
 
 #[no_mangle]
-pub extern "C" fn android_main(_app: slint::android::AndroidApp) {
+pub extern "C" fn android_main(app: slint::android::AndroidApp) {
+    // Inisialisasi UI
     let ui = AppWindow::new().unwrap();
+    
+    // Slint butuh tahu 'app' mana yang mengaturnya di Android
+    slint::android::init(app).unwrap();
+    
     ui.run().unwrap();
 }
