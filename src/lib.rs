03ae8c8779782377596466f474284d9b492eea93
invuzt <invuzt@gmail.com>
2026-04-12 04:27:25 +0700

    UI: Added camera intent button and status logger

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,23 @@ pub extern "C" fn android_main(app: slint::android::AndroidApp) {
     let ui = AppWindow::new().unwrap();
     let ui_handle = ui.as_weak();
 
-    ui.on_process_data(move |info| {
+    // Callback Tombol Kamera
+    ui.on_open_camera(move || {
         let ui = ui_handle.unwrap();
-        let log = format!("Sukses: {}", info);
-        ui.set_status_text(log.into());
+        ui.set_status_text("Membuka kamera sistem...".into());
+        
+        // Di sini biasanya kita panggil JNI untuk:
+        // let intent = Intent::new("android.media.action.IMAGE_CAPTURE");
+        // activity.startActivityForResult(intent, REQUEST_CODE);
+        
+        // Untuk simulasi sat-set:
+        println!("Log: Intent Kamera Dipicu");
+    });
+
+    let ui_handle_2 = ui.as_weak();
+    ui.on_process_data(move |info| {
+        let ui = ui_handle_2.unwrap();
+        ui.set_status_text(format!("Data: {}", info).into());
     });
 
     ui.run().unwrap();
