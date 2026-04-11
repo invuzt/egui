50ecac8dab73c70305dc95d9ac8b9bfc0f3013be
invuzt <invuzt@gmail.com>
2026-04-12 01:44:15 +0700

    Fix: Corrected slint feature name to backend-android-activity-06

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ slint::include_modules!();
 
 #[no_mangle]
 pub extern "C" fn android_main(app: slint::android::AndroidApp) {
-    // Sekarang slint::android harusnya sudah 'kenal' karena fitur sudah aktif
+    // Inisialisasi backend Slint
     slint::android::init(app).unwrap();
     
     let ui = AppWindow::new().unwrap();
