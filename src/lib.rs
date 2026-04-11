1447fbc5ed448572b867b221f3cb95a4c538929d
invuzt <invuzt@gmail.com>
2026-04-12 03:38:33 +0700

    UI: Showcase native-style components (Slider, Toggle, Password, ScrollView)

--- src/lib.rs
+++ src/lib.rs
@@ -7,19 +7,10 @@ pub extern "C" fn android_main(app: slint::android::AndroidApp) {
     let ui = AppWindow::new().unwrap();
     let ui_handle = ui.as_weak();
 
-    // Logika ketika tombol diklik
-    ui.on_process_data(move |input| {
+    ui.on_process_data(move |info| {
         let ui = ui_handle.unwrap();
-        
-        // Contoh proses sederhana: Jika input kosong beri peringatan
-        if input.trim().is_empty() {
-            ui.set_status_text("Error: Input tidak boleh kosong!".into());
-        } else {
-            // Ubah teks status berdasarkan input
-            let hasil = format!("Data '{}' berhasil diproses ke sistem.", input);
-            ui.set_status_text(hasil.into());
-            ui.set_input_text("".into()); // Reset input setelah diproses
-        }
+        let log = format!("Sukses: {}", info);
+        ui.set_status_text(log.into());
     });
 
     ui.run().unwrap();
