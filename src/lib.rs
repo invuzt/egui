b9db56434a3ff659aa85a9a0a9069389769cd0bb
invuzt <invuzt@gmail.com>
2026-04-12 01:38:16 +0700

    Fix: Explicitly add android-activity to resolve unresolved crate error

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,8 @@
 slint::include_modules!();
 
-// Memastikan linker menyertakan backend android
-#[cfg(target_os = "android")]
-use i_slint_backend_android_activity as _;
-
 #[no_mangle]
 pub extern "C" fn android_main(app: slint::android::AndroidApp) {
-    // Inisialisasi wajib untuk Slint di Android
+    // Inisialisasi wajib untuk Slint Android
     slint::android::init(app).unwrap();
     
     let ui = AppWindow::new().unwrap();
