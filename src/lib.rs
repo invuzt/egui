bc746bd4b87501c8c33e1ca3b5659b028d8ebae5
invuzt <invuzt@gmail.com>
2026-04-11 20:53:35 +0700

    Fix: Force specific Slint version and link android backend

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 slint::include_modules!();
 
+// Memastikan backend android ikut ter-link
+#[cfg(target_os = "android")]
+use i_slint_backend_android_activity as _;
+
 #[no_mangle]
 pub extern "C" fn android_main(_app: slint::android::AndroidApp) {
-    // Inisialisasi UI
     let ui = AppWindow::new().unwrap();
-    
-    // Jalankan event loop
     ui.run().unwrap();
 }
