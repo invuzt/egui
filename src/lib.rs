dbc16971b94dd2f350d0a9d9359dc65443a9ae1e
invuzt <invuzt@gmail.com>
2026-04-12 01:57:16 +0700

    Fix: Slint backend features and Android SDK platform path

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,7 @@ slint::include_modules!();
 
 #[no_mangle]
 pub extern "C" fn android_main(app: slint::android::AndroidApp) {
-    // Inisialisasi backend Slint
     slint::android::init(app).unwrap();
-    
     let ui = AppWindow::new().unwrap();
     ui.run().unwrap();
 }
