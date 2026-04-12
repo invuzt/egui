c2631a4d694d621ce2264dc039b0f2bc737af2fa
invuzt <invuzt@gmail.com>
2026-04-12 12:23:57 +0700

    Fix Cargo features and expanded widget gallery

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@ slint::include_modules!();
 #[no_mangle]
 pub extern "C" fn android_main(app: slint::android::AndroidApp) {
     slint::android::init(app).unwrap();
-    
     let ui = AppWindow::new().unwrap();
     ui.run().unwrap();
 }
