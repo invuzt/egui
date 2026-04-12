66fa376fce9150b57e78d15b55b8f2520ff9d084
invuzt <invuzt@gmail.com>
2026-04-12 12:58:38 +0700

    Showcase: Added Switch, Tabs, and interconnected UI status

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ fn main() {
     slint_build::compile_with_config(
             "src/main_ui.slint",
                     slint_build::CompilerConfiguration::new()
-                                .with_style("cupertino".into())
+                                .with_style("fluent".into())
                                     ).unwrap();
                                     }
                                     
